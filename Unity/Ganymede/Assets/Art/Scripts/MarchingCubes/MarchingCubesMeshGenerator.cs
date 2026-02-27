f10f8407925834fb1932fab65ba904252b94fd8d
Bassem-Adam <basimsherif.cbs.2020@gmail.com>
2026-02-27 23:05:38 +0200

    Implement water mesh generation in GeneraterMesh method and add flatten3DArray helper function

--- Unity/Ganymede/Assets/Art/Scripts/MarchingCubes/MarchingCubesMeshGenerator.cs
+++ Unity/Ganymede/Assets/Art/Scripts/MarchingCubes/MarchingCubesMeshGenerator.cs
@@ -36,7 +36,62 @@ public class MarchingCubesMeshGenerator : MonoBehaviour
     [ContextMenu("Generate Water Mesh")]
     public void GeneraterMesh()
     {
+        // get res will need it to dimension compute shader dispatch and buffer sizes
+        Vector3Int res = volumeManager.gridResolution;
 
+        // generate SDF grid
+        sdfGenerator.GenerateSDF();
+        float[,,] grid3D = sdfGenerator.GetSDFGrid();
+        float[] flatGrid = flatten3DArray(grid3D); // compute buffers carry 1d array
+
+    
+        // allocate & set data to compute shader Buffers
+        ComputeBuffer sdfBuffer = new ComputeBuffer(flatGrid.Length, sizeof(float));
+        sdfBuffer.SetData(flatGrid);
+
+        // allocate Output buffer for triangles
+        // a cube can have maximum in worst case 5 triangles 
+        // max triangles = number of voxels * 5
+        // number of voxels determined by grid resolution - 1 in each dimension
+        // also dont forget each traignle have 3 verticies each vertex is a Vector3 (3 floats)
+        // so so 5*9 floats per voxel
+        int maxTriangles = res.x * res.y * res.z * 5;
+        ComputeBuffer triangleBuffer = new ComputeBuffer(maxTriangles, sizeof(float) * 9, ComputeBufferType.Append);
+        triangleBuffer.SetCounterValue(0);
+
+        // Load data to compute shader
+        int kernel = marchingShader.FindKernel("March");
+        marchingShader.SetBuffer(kernel, "SDFGrid", sdfBuffer); // input
+        marchingShader.SetVector("GridSize", new Vector3(res.x, res.y, res.z)); // input
+        marchingShader.SetVector("VoxelSize", volumeManager.GetVoxelSize()); // input
+        marchingShader.SetFloat("isoLevel", isoLevel); // input
+        marchingShader.SetBuffer(kernel, "OutputTriangles", triangleBuffer); // output
+
+        // Dispatch compute shader
+        int threadGroupsX = Mathf.CeilToInt(res.x / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(res.y / 8.0f);
+        int threadGroupsZ = Mathf.CeilToInt(res.z / 8.0f);
+        marchingShader.Dispatch(kernel, threadGroupsX, threadGroupsY, threadGroupsZ);
+
+        // Retrieve triangle data from GPU
+        // since its an append so we dont really know how many triangles were generated 
+        ComputeBuffer argBuffer = new ComputeBuffer(4, sizeof(int), ComputeBufferType.IndirectArguments); 
+        ComputeBuffer.CopyCount(triangleBuffer, argBuffer, 0); // copy the count of triangles generated to argBuffer
+        int[] args = new int[4];
+        argBuffer.GetData(args);
+        int triangleCount = args[0]; // the count of triangles generated
+
+        // now lets get or read the exact triangle data from the GPU
+        Triangle[] gpuTriangles = new Triangle[triangleCount];
+        triangleBuffer.GetData(gpuTriangles, 0, 0, triangleCount); 
+
+        // BUILD THE UNITY MESH XD
+        ConstructUnityMesh(gpuTriangles);
+
+        // Clean up
+        sdfBuffer.Release();
+        triangleBuffer.Release();
+        argBuffer.Release();
     }
 
     private void ConstructUnityMesh(Triangle[] triangles)
@@ -44,4 +99,29 @@ public class MarchingCubesMeshGenerator : MonoBehaviour
         
     }
 
+
+    //helpers
+
+    private float[] flatten3DArray(float[,,] array3D)
+    {
+        int xSize = array3D.GetLength(0);
+        int ySize = array3D.GetLength(1);
+        int zSize = array3D.GetLength(2);
+
+        float[] flatArray = new float[xSize * ySize * zSize];
+        int index = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                for (int x = 0; x < xSize; x++)
+                {
+                    flatArray[index++] = array3D[x, y, z];
+                }
+            }
+        }
+
+        return flatArray;
+    }
 }
